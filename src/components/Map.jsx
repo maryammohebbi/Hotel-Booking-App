2ca20ccab70f01de9fa359d476e2597b2aab5418
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2023-11-21 00:14:02 +0330

    react leaflet package for having map installed and its components got used

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHotel } from '../context/HotelProvider'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
 
 function Map() {
     
    const {isLoading, hotels} = useHotel()
+   const [position, setPosition] = useState([51.505, -0.09])
   return (
     <div className=' bg-green-400 h-52 lg:w-[65%] lg:h-full'>
-        map
+        <MapContainer className='h-full' center={position} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
     </div>
   )
 }
