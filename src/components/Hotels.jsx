fbda5a58ba975c4cdbc7c7ef23b4b5c40147632f
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2023-11-22 01:24:05 +0330

    which hotel was clicked the last time added

--- src/components/Hotels.jsx
+++ src/components/Hotels.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { useHotel } from '../context/HotelProvider'
 
 function Hotels() {
-    const {isLoading, hotels} = useHotel()
+    const {isLoading, hotels, currentHotel} = useHotel()
 
     if(isLoading) return <p>Loading...</p>
   return (
@@ -13,8 +13,8 @@ function Hotels() {
             {
                 hotels.map(item => (
                     <Link to={`/hotels/${item.id}?lat=${item.latitude}&lng=${item.longitude}`} key={item.id}>
-                        <div className='flex border-2 border-slate-500 rounded-lg shadow-xl w-full h-28' >
-                            <img src={item.picture_url.url} alt={item.name} className='w-[35%]'/>
+                        <div className={`flex border-2 border-slate-500 rounded-lg shadow-xl w-full h-28 p-1 ${item.id === currentHotel?.id ? "border-green-500" : ""}`} >
+                            <img src={item.picture_url.url} alt={item.name} className='w-[35%] rounded-lg'/>
                             <div className='p-2 w-[65%] flex flex-col justify-center'>
                                 <p className='font-bold'>{item.smart_location}</p>
                                 <p className='text-sm font-bold text-slate-800'>{item.name}</p>
