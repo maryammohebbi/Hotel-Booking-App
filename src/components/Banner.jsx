b2f0ee77f7f6cddaaf8e1118d75a9f9945b86c23
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2023-11-23 04:39:53 +0330

    simple typewrite package installed and got used in the banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Header from './Header'
 import {motion} from "framer-motion"
+import {Typewriter} from "react-simple-typewriter"
 
 function Banner() {
   return (
     <div className='w-full relative mb-8'>
-        <img src="../src/assets/b.jpg" alt="" className='w-full h-[30rem] object-cover'/>
-        <p className='text-3xl top-[30%] left-16 md:text-4xl lg:text-5xl text-white font-bold absolute lg:top-[40%] lg:left-32 drop-shadow-2xl w-full'>
+        <img src="../src/assets/b.jpg" alt="banner" className='w-full h-[30rem] object-cover'/>
+        <p className='text-3xl p-4 top-[30%] md:text-4xl lg:text-5xl text-white font-bold absolute lg:top-[40%] drop-shadow-2xl w-full'>
           WELCOME TO THE 
           <br/>
-          <span className='absolute left-6 lg:left-12'>HOTEL BOOKING WEBSITE</span>
+          <Typewriter words={["HOTEL BOOKING WEBSITE"]} loop={5} />
         </p>
         <div className='md:absolute md:top-[90%] w-full'><Header/></div>  
     </div>
