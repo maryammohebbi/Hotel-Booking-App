19a84dc5f1dc89df1d8ea1f1f09c16ec390d8046
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2023-12-22 23:11:43 +0330

    banner photo relocated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ import {Typewriter} from "react-simple-typewriter"
 function Banner() {
   return (
     <div className='w-full relative mb-8'>
-        <img src="../src/assets/b.jpg" alt="banner" className='w-full h-[30rem] object-cover'/>
+        <img src="/b.jpg" alt="banner" className='w-full h-[30rem] object-cover'/>
         <p className='text-3xl p-4 top-[30%] md:text-4xl lg:text-5xl text-white font-bold absolute lg:top-[40%] drop-shadow-2xl w-full'>
           WELCOME TO THE 
           <br/>
